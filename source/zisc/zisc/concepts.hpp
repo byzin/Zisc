d0a6668b75abee3973ca2ed4204b6bbd3477ecf1
Sho Ikeda <zin.kosmos.1190@gmail.com>
2020-10-01 23:27:48 +0900

    Simplify concepts.

--- source/zisc/zisc/concepts.hpp
+++ source/zisc/zisc/concepts.hpp
@@ -15,7 +15,15 @@
 #ifndef ZISC_CONCEPTS_HPP
 #define ZISC_CONCEPTS_HPP
 
-#include "concepts-inl.hpp"
+// Check compiler
+#if !defined(__cpp_concepts)
+static_assert(false, "'Concepts' isn't supported in the compiler.")
+#endif
+
+// Standard C++ library
+#include <functional>
+#include <type_traits>
+#include <utility>
 
 namespace zisc {
 
@@ -23,53 +31,55 @@ namespace zisc {
 
 //! Specify a type is the same as another type
 template <typename T, typename U>
-concept SameAs = inner::SameAs<T, U>;
+concept SameAs = std::is_same_v<U, T>;
 
 //! Specify a type is derived from another type
 template <typename Derived, typename Base>
-concept DerivedFrom = inner::DerivedFrom<Derived, Base>;
+concept DerivedFrom = std::is_base_of_v<Base, Derived>;
 
 //! Specify a type is an integer type
 template <typename Type>
-concept Integer = inner::Integer<Type>;
+concept Integer = std::is_integral_v<Type> && !SameAs<bool, Type>;
 
 //! Specify a type is an integer type which is signed
 template <typename Type>
-concept SignedInteger = inner::SignedInteger<Type>;
+concept SignedInteger = Integer<Type> && std::is_signed_v<Type>;
 
 //! Specify a type is an integer type which is unsigned
 template <typename Type>
-concept UnsignedInteger = inner::UnsignedInteger<Type>;
+concept UnsignedInteger = Integer<Type> && std::is_unsigned_v<Type>;
 
 //! Specify a type is a floating-point type
 template <typename Type>
-concept FloatingPoint = inner::FloatingPoint<Type>;
+concept FloatingPoint = std::is_floating_point_v<Type>;
 
 //! Specify a type is a pointer
 template <typename Type>
-concept Pointer = inner::Pointer<Type>;
+concept Pointer = std::is_pointer_v<Type>;
 
 //! Specify a type is arithmetic
 template <typename Type>
-concept Arithmetic = inner::Arithmetic<Type>;
+concept Arithmetic = std::is_arithmetic_v<Type>;
 
 //! Specify a type is scalar
 template <typename Type>
-concept Scalar = inner::Scalar<Type>;
+concept Scalar = std::is_scalar_v<Type>;
 
 //! Specify a type is trivial
 template <typename Type>
-concept Trivial = inner::Trivial<Type>;
+concept Trivial = std::is_trivial_v<Type>;
 
 //! Specify a type is trivially copyable
 template <typename Type>
-concept TriviallyCopyable = inner::TriviallyCopyable<Type>;
+concept TriviallyCopyable = std::is_trivially_copyable_v<Type>;
 
 // Callable concepts
 
 //! Specify a callable type can be invoked with a given set of argument
 template <typename Func, typename ...Args>
-concept Invocable = inner::Invocable<Func, Args...>;
+concept Invocable = requires(Func&& func, Args&&... args) {
+  std::invoke(std::forward<Func>(func), std::forward<Args>(args)...);
+};
 
 } // namespace zisc
 

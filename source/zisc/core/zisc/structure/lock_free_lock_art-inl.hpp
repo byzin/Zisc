b9711b0ec60251e72522891b979e0d52ccbc799f
Sho Ikeda <zin.kosmos.1190@gmail.com>
2022-05-17 02:02:22 +0900

    Add initial files.

---
+++ source/zisc/core/zisc/structure/lock_free_lock_art-inl.hpp
@@ -0,0 +1,499 @@
+/*!
+  \file lock_free_lock_art-inl.hpp
+  \author Sho Ikeda
+  \brief No brief description
+
+  \details
+  No detailed description.
+
+  \copyright
+  Copyright (c) 2015-2022 Sho Ikeda
+  This software is released under the MIT License.
+  http://opensource.org/licenses/mit-license.php
+  */
+
+#ifndef ZISC_LOCK_FREE_LOCK_ART_INL_HPP
+#define ZISC_LOCK_FREE_LOCK_ART_INL_HPP
+
+#include "lock_free_lock_art.hpp"
+// Standard C++ library
+#include <algorithm>
+#include <concepts>
+#include <cstddef>
+#include <limits>
+#include <memory>
+#include <mutex>
+#include <numeric>
+#include <optional>
+#include <shared_mutex>
+#include <utility>
+#include <vector>
+// Zisc
+#include "container_overflow_error.hpp"
+#include "map.hpp"
+#include "zisc/utility.hpp"
+#include "zisc/zisc_config.hpp"
+#include "zisc/memory/data_storage.hpp"
+#include "zisc/memory/std_memory_resource.hpp"
+
+namespace zisc {
+
+/*!
+  \details No detailed description
+
+  \param [in,out] mem_resource No description.
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+LockFreeLockArt<Key, T, Compare>::LockFreeLockArt(pmr::memory_resource* mem_resource) noexcept :
+    LockFreeLockArt(1, mem_resource)
+{
+}
+
+/*!
+  \details No detailed description
+
+  \param [in] cap No description.
+  \param [in,out] mem_resource No description.
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+LockFreeLockArt<Key, T, Compare>::LockFreeLockArt(const size_type cap,
+                                    pmr::memory_resource* mem_resource) noexcept :
+    index_stack_{typename decltype(index_stack_)::allocator_type{mem_resource}},
+    node_pool_{typename decltype(node_pool_)::allocator_type{mem_resource}},
+    node_list_{typename decltype(node_list_)::allocator_type{mem_resource}}
+{
+  setCapacity(cap);
+}
+
+/*!
+  \details No detailed description
+
+  \param [in,out] other No description.
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+LockFreeLockArt<Key, T, Compare>::LockFreeLockArt(LockFreeLockArt&& other) noexcept :
+    index_stack_{std::move(other.index_stack_)},
+    node_pool_{std::move(other.node_pool_)},
+    node_list_{std::move(other.node_list_)}
+{
+}
+
+/*!
+  \details No detailed description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+LockFreeLockArt<Key, T, Compare>::~LockFreeLockArt() noexcept
+{
+  clear();
+}
+
+/*!
+  \details No detailed description
+
+  \param [in,out] other No description.
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+auto LockFreeLockArt<Key, T, Compare>::operator=(LockFreeLockArt&& other) noexcept -> LockFreeLockArt&
+{
+  clear();
+  BaseMapT::operator=(std::move(other));
+  index_stack_ = std::move(other.index_stack_);
+  node_pool_ = std::move(other.node_pool_);
+  node_list_ = std::move(other.node_list_);
+  return *this;
+}
+
+/*!
+  \details No detailed description
+
+  \param [in] value No description.
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+template <typename ...Args> inline
+auto LockFreeLockArt<Key, T, Compare>::add(Args&&... args) -> std::optional<size_type>
+    requires std::is_nothrow_constructible_v<ValueT, Args...>
+{
+  ValueT value{std::forward<Args>(args)...};
+  ConstKeyT& key = BaseMapT::getKey(value);
+  size_type node_index = invalidId();
+  {
+    std::unique_lock lock{mutex_};
+    pmr::vector<StoragePtr>& list = node_list_;
+    auto pos = std::lower_bound(list.begin(), list.end(), key, LockFreeLockArt::compare);
+    if ((pos == list.end()) || !LockFreeLockArt::equal(*pos, key)) {
+      // 
+      using OverflowErr = typename BaseMapT::OverflowError;
+      if (index_stack_.empty()) {
+        const char* message = "Bst overflow happened.";
+        throw OverflowErr{message, resource(), std::move(value)};
+      }
+      //
+      node_index = issueStorageIndex();
+      StorageRef storage = getStorage(node_index);
+      storage.set(std::move(value));
+      list.emplace(pos, std::addressof(storage));
+    }
+  }
+  return (node_index != invalidId())
+      ? std::make_optional(node_index)
+      : std::optional<size_type>{};
+}
+
+/*!
+  \details No detailed description
+
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+auto LockFreeLockArt<Key, T, Compare>::capacity() const noexcept -> size_type
+{
+  const size_type cap  = node_pool_.size();
+  return cap;
+}
+
+/*!
+  \details No detailed description
+
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+constexpr auto LockFreeLockArt<Key, T, Compare>::capacityMax() noexcept -> size_type
+{
+  const size_type cap = (std::numeric_limits<size_type>::max)() >> 1;
+  return cap;
+}
+
+/*!
+  \details No detailed description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+void LockFreeLockArt<Key, T, Compare>::clear() noexcept
+{
+  // Skip clear operation after moving data to ather 
+  if (node_pool_.empty())
+    return;
+
+  std::for_each(node_list_.begin(), node_list_.end(), [](StoragePtr ptr) noexcept
+  {
+    ptr->destroy();
+  });
+  node_list_.clear();
+  std::iota(index_stack_.rbegin(), index_stack_.rend(), 0);
+}
+
+/*!
+  \details No detailed description
+
+  \param [in] key No description.
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+auto LockFreeLockArt<Key, T, Compare>::contain(ConstKeyT& key) const noexcept
+    -> std::optional<size_type>
+{
+  size_type node_index = invalidId();
+  {
+    std::shared_lock lock{mutex_};
+    const pmr::vector<StoragePtr>& list = node_list_;
+    auto pos = std::lower_bound(list.begin(), list.end(), key, LockFreeLockArt::compare);
+    if ((pos != list.end()) && LockFreeLockArt::equal(*pos, key))
+      node_index = getIndex(*pos);
+  }
+  return (node_index != invalidId())
+      ? std::make_optional(node_index)
+      : std::optional<size_type>();
+}
+
+/*!
+  \details No detailed description
+
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+auto LockFreeLockArt<Key, T, Compare>::findMinKey() noexcept -> std::optional<Pointer>
+{
+  const pmr::vector<StoragePtr>& list = node_list_;
+  std::shared_lock lock{mutex_};
+  ZISC_ASSERT(std::is_sorted(list.begin(), list.end(), LockFreeLockArt::compareNode),
+              "The node list isn't sorted.");
+  return !list.empty()
+      ? std::make_optional(node_list_.front()->memory())
+      : std::optional<Pointer>{};
+}
+
+/*!
+  \details No detailed description
+
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+auto LockFreeLockArt<Key, T, Compare>::findMinKey() const noexcept -> std::optional<ConstPointer>
+{
+  const pmr::vector<StoragePtr>& list = node_list_;
+  std::shared_lock lock{mutex_};
+  ZISC_ASSERT(std::is_sorted(list.begin(), list.end(), LockFreeLockArt::compareNode),
+              "The node list isn't sorted.");
+  return !list.empty()
+      ? std::make_optional(node_list_.front()->memory())
+      : std::optional<ConstPointer>{};
+}
+
+/*!
+  \details No detailed description
+
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+auto LockFreeLockArt<Key, T, Compare>::get(const size_type index) noexcept
+    -> Reference
+{
+  return *getStorage(index);
+}
+
+/*!
+  \details No detailed description
+
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+auto LockFreeLockArt<Key, T, Compare>::get(const size_type index) const noexcept
+    -> ConstReference
+{
+  return *getStorage(index);
+}
+
+/*!
+  \details No detailed description
+
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+constexpr bool LockFreeLockArt<Key, T, Compare>::isBounded() noexcept
+{
+  return true;
+}
+
+/*!
+  \details No detailed description
+
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+constexpr bool LockFreeLockArt<Key, T, Compare>::isConcurrent() noexcept
+{
+  return true;
+}
+
+/*!
+  \details No detailed description
+
+  \param [in] key No description.
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+auto LockFreeLockArt<Key, T, Compare>::remove(ConstKeyT& key) -> std::optional<size_type>
+{
+  size_type node_index = invalidId();
+  {
+    std::unique_lock lock{mutex_};
+    pmr::vector<StoragePtr>& list = node_list_;
+    auto pos = std::lower_bound(list.begin(), list.end(), key, LockFreeLockArt::compare);
+    if ((pos != list.end()) && LockFreeLockArt::equal(*pos, key)) {
+      node_index = getIndex(*pos);
+      (*pos)->destroy();
+      list.erase(pos);
+      index_stack_.emplace_back(node_index);
+    }
+  }
+  return (node_index != invalidId())
+      ? std::make_optional(node_index)
+      : std::optional<size_type>{};
+}
+
+/*!
+  \details No detailed description
+
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+pmr::memory_resource* LockFreeLockArt<Key, T, Compare>::resource() const noexcept
+{
+  pmr::memory_resource* mem_resource = node_pool_.get_allocator().resource();
+  return mem_resource;
+}
+
+/*!
+  \details No detailed description
+
+  \param [in] cap No description.
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+void LockFreeLockArt<Key, T, Compare>::setCapacity(size_type cap) noexcept
+{
+  constexpr size_type lowest_size = 1;
+  cap = (std::max)(lowest_size, cap);
+
+  clear();
+  index_stack_.resize(cap);
+  node_pool_.resize(index_stack_.size());
+  node_list_.reserve(index_stack_.size());
+  clear();
+}
+
+/*!
+  \details No detailed description
+
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+auto LockFreeLockArt<Key, T, Compare>::size() const noexcept -> size_type
+{
+  size_type s = 0;
+  {
+    std::shared_lock lock{mutex_};
+    s = node_list_.size();
+  }
+  return s;
+}
+
+/*!
+  \details No detailed description
+
+  \param [in] lhs No description.
+  \param [in] rhs No description.
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+bool LockFreeLockArt<Key, T, Compare>::compare(ConstStoragePtr lhs, ConstKeyT& rhs) noexcept
+{
+  ConstKeyT& lhs_key = BaseMapT::getKey(lhs->get());
+  const bool result = CompareT{}(lhs_key, rhs);
+  return result;
+}
+
+/*!
+  \details No detailed description
+
+  \param [in] lhs No description.
+  \param [in] rhs No description.
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+bool LockFreeLockArt<Key, T, Compare>::compareNode(ConstStoragePtr lhs, ConstStoragePtr rhs) noexcept
+{
+  return LockFreeLockArt::compare(lhs, BaseMapT::getKey(rhs->get()));
+}
+
+/*!
+  \details No detailed description
+
+  \param [in] lhs No description.
+  \param [in] rhs No description.
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+bool LockFreeLockArt<Key, T, Compare>::equal(ConstStoragePtr lhs, ConstKeyT& rhs) noexcept
+{
+  ConstKeyT& lhs_key = BaseMapT::getKey(lhs->get());
+  const bool result = !CompareT{}(lhs_key, rhs) && !CompareT{}(rhs, lhs_key);
+  return result;
+}
+
+/*!
+  \details No detailed description
+
+  \param [in] node No description.
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+auto LockFreeLockArt<Key, T, Compare>::getIndex(ConstStoragePtr node) const noexcept -> size_type
+{
+  ConstStoragePtr head = node_pool_.data();
+  const auto index = cast<size_type>(std::distance(head, node));
+  return index;
+}
+
+/*!
+  \details No detailed description
+
+  \param [in] index No description.
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+auto LockFreeLockArt<Key, T, Compare>::getStorage(const size_type index) noexcept
+    -> StorageRef
+{
+  return node_pool_[index];
+}
+
+/*!
+  \details No detailed description
+
+  \param [in] index No description.
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+auto LockFreeLockArt<Key, T, Compare>::getStorage(const size_type index) const noexcept
+    -> ConstStorageRef
+{
+  return node_pool_[index];
+}
+
+/*!
+  \details No detailed description
+
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+constexpr auto LockFreeLockArt<Key, T, Compare>::invalidId() noexcept -> size_type
+{
+  constexpr size_type invalid = (std::numeric_limits<size_type>::max)();
+  return invalid;
+}
+
+/*!
+  \details No detailed description
+
+  \return No description
+  */
+template <std::movable Key, MappedValue T, std::invocable<Key, Key> Compare>
+inline
+auto LockFreeLockArt<Key, T, Compare>::issueStorageIndex() noexcept -> size_type
+{
+  ZISC_ASSERT(!index_stack_.empty(), "The index stack is empty.");
+  const size_type index = index_stack_.back();
+  index_stack_.pop_back();
+  return index;
+}
+
+} // namespace zisc
+
+#endif // ZISC_LOCK_FREE_LOCK_ART_INL_HPP
---
+++ source/zisc/core/zisc/structure/lock_free_lock_art.hpp
@@ -0,0 +1,186 @@
+/*!
+  \file lock_free_lock_art.hpp
+  \author Sho Ikeda
+  \brief No brief description
+
+  \details
+  No detailed description.
+
+  \copyright
+  Copyright (c) 2015-2022 Sho Ikeda
+  This software is released under the MIT License.
+  http://opensource.org/licenses/mit-license.php
+  */
+
+#ifndef ZISC_LOCK_FREE_LOCK_ART_HPP
+#define ZISC_LOCK_FREE_LOCK_ART_HPP
+
+// Standard C++ library
+#include <concepts>
+#include <cstddef>
+#include <functional>
+#include <optional>
+#include <shared_mutex>
+#include <type_traits>
+#include <utility>
+#include <vector>
+// Zisc
+#include "map.hpp"
+#include "zisc/zisc_config.hpp"
+#include "zisc/memory/data_storage.hpp"
+#include "zisc/memory/std_memory_resource.hpp"
+
+namespace zisc {
+
+/*!
+  \brief Lock Free Lock Adaptive Radix Tree
+
+  For more details, please see the following paper:
+  <a href="https://arxiv.org/abs/2201.00813">Lock-Free Locks Revisited</a>. <br>
+
+  \tparam Key No description.
+  \tparam T No description.
+  \tparam Compare No description.
+  */
+template <std::movable Key,
+          MappedValue T = void,
+          std::invocable<Key, Key> Compare = std::less<Key>>
+class LockFreeLockArt : public Map<LockFreeLockArt<Key, T, Compare>, Key, T, Compare>
+{
+ public:
+  // Type aliases
+  using BaseMapT = Map<LockFreeLockArt<Key, T, Compare>, Key, T, Compare>;
+  using KeyT = typename BaseMapT::KeyT;
+  using ConstKeyT = typename BaseMapT::ConstKeyT;
+  using MappedT = typename BaseMapT::MappedT;
+  using ValueT = typename BaseMapT::ValueT;
+  using ConstValueT = typename BaseMapT::ConstValueT;
+  using CompareT = typename BaseMapT::CompareT;
+  using Reference = typename BaseMapT::Reference;
+  using RReference = typename BaseMapT::RReference;
+  using ConstReference = typename BaseMapT::ConstReference;
+  using Pointer = typename BaseMapT::Pointer;
+  using ConstPointer = typename BaseMapT::ConstPointer;
+
+  // Type aliases for STL
+  using key_type = typename BaseMapT::key_type;
+  using mapped_type = typename BaseMapT::mapped_type;
+  using value_type = typename BaseMapT::value_type;
+  using size_type = typename BaseMapT::size_type;
+  using difference_type = typename BaseMapT::difference_type;
+  using key_compare = typename BaseMapT::key_compare;
+  using reference = typename BaseMapT::reference;
+  using const_reference = typename BaseMapT::const_reference;
+  using pointer = typename BaseMapT::pointer;
+  using const_pointer = typename BaseMapT::const_pointer;
+
+
+  //! Create a bst
+  LockFreeLockArt(pmr::memory_resource* mem_resource) noexcept;
+
+  //! Create a bst
+  LockFreeLockArt(const size_type cap, pmr::memory_resource* memory_resource) noexcept;
+
+  //! Move a data
+  LockFreeLockArt(LockFreeLockArt&& other) noexcept;
+
+  //! Destroy the bst
+  ~LockFreeLockArt() noexcept;
+
+
+  //! Move a data
+  LockFreeLockArt& operator=(LockFreeLockArt&& other) noexcept;
+
+
+  //! Insert the given value into the bst
+  template <typename ...Args>
+  [[nodiscard]] std::optional<size_type> add(Args&&... args)
+      requires std::is_nothrow_constructible_v<ValueT, Args...>;
+
+  //! Return the maximum possible number of elements
+  size_type capacity() const noexcept;
+
+  //! Return the maximum possible capacity
+  static constexpr size_type capacityMax() noexcept;
+
+  //! Clear the contents
+  void clear() noexcept;
+
+  //! Check if the given value is contained in the bst
+  [[nodiscard]] std::optional<size_type> contain(ConstKeyT& key) const noexcept;
+
+  //! Find the minimum key in the bst
+  [[nodiscard]] std::optional<Pointer> findMinKey() noexcept;
+
+  //! Find the minimum key in the bst
+  [[nodiscard]] std::optional<ConstPointer> findMinKey() const noexcept;
+
+  //! Return the value by the given index
+  Reference get(const size_type index) noexcept;
+
+  //! Return the value by the given index
+  ConstReference get(const size_type index) const noexcept;
+
+  //! Check if the bst is bounded
+  static constexpr bool isBounded() noexcept;
+
+  //! Check if the bst is concurrent
+  static constexpr bool isConcurrent() noexcept;
+
+  //! Remove the value from the bst
+  [[nodiscard]] std::optional<size_type> remove(ConstKeyT& key);
+
+  //! Return a pointer to the underlying memory resource
+  pmr::memory_resource* resource() const noexcept;
+
+  //! Change the maximum possible number of elements. The bst will be cleared
+  void setCapacity(size_type cap) noexcept;
+
+  //! Return the number of elements in the bst
+  size_type size() const noexcept;
+
+ private:
+  using StorageT = DataStorage<ValueT>;
+  using ConstStorageT = std::add_const_t<StorageT>;
+  using StorageRef = std::add_lvalue_reference_t<StorageT>;
+  using ConstStorageRef = std::add_lvalue_reference_t<ConstStorageT>;
+  using StoragePtr = std::add_pointer_t<StorageT>;
+  using ConstStoragePtr = std::add_pointer_t<ConstStorageT>;
+
+
+  //! Compare the two keys
+  static bool compare(ConstStoragePtr lhs, ConstKeyT& rhs) noexcept;
+
+  //! Compare the two keys of nodes
+  static bool compareNode(ConstStoragePtr lhs, ConstStoragePtr rhs) noexcept;
+
+  //! Check if the two given keys are same
+  static bool equal(ConstStoragePtr lhs, ConstKeyT& rhs) noexcept;
+
+  //! Return the index of the given node
+  size_type getIndex(ConstStoragePtr node) const noexcept;
+
+  //! Return the storage by the given index
+  StorageRef getStorage(const size_type index) noexcept;
+
+  //! Return the storage by the given index
+  ConstStorageRef getStorage(const size_type index) const noexcept;
+
+  //! Return the invalid id
+  static constexpr size_type invalidId() noexcept;
+
+  //! Issue a storage index from the index stack
+  size_type issueStorageIndex() noexcept;
+
+
+  mutable std::shared_mutex mutex_;
+  pmr::vector<size_type> index_stack_;
+  pmr::vector<StorageT> node_pool_;
+  pmr::vector<StoragePtr> node_list_;
+};
+
+} // namespace zisc
+
+#include "lock_free_lock_art-inl.hpp"
+
+#endif // ZISC_LOCK_FREE_LOCK_ART_HPP

bd08f4d6a2321ffde797d1e4539bce4d11823ded
Sho Ikeda <zin.kosmos.1190@gmail.com>
2023-01-02 12:08:36 +0900

    Update copyright.

--- test/unittest/concurrent_queue_test.cpp
+++ test/unittest/concurrent_queue_test.cpp
@@ -7,7 +7,7 @@
   No detailed description.
 
   \copyright
-  Copyright (c) 2015-2022 Sho Ikeda
+  Copyright (c) 2015-2023 Sho Ikeda
   This software is released under the MIT License.
   http://opensource.org/licenses/mit-license.php
   */

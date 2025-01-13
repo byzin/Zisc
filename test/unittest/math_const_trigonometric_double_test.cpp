50e140922e00a0d22d44cb10ce7c20d7db0e939d
Sho Ikeda <zin.kosmos.1190@gmail.com>
2025-01-13 22:25:51 +0900

    Update copyright.

--- test/unittest/math_const_trigonometric_double_test.cpp
+++ test/unittest/math_const_trigonometric_double_test.cpp
@@ -7,7 +7,7 @@
 //  No detailed description.
 //
 //  \copyright
-//  Copyright (c) 2015-2024 Sho Ikeda
+//  Copyright (c) 2015-2025 Sho Ikeda
 //  This software is released under the MIT License.
 //  http://opensource.org/licenses/mit-license.php
 //  */
